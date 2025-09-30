bd7a3954d2a431834c08a6226d4fd957319b5a17
Mikita Shket <90976471+screenshifter@users.noreply.github.com>
2025-09-30 23:14:22 +0200

    REFACT(credit_length_estimator): Merge all simple credit functions

--- finance/optimal_credit_length_estimation/credit_length_estimator.py
+++ finance/optimal_credit_length_estimation/credit_length_estimator.py
@@ -5,11 +5,14 @@ from python.runfiles import runfiles
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 
 from typing import Any
-from finance.optimal_credit_length_estimation.detail.input import parse_input, validate_input
-from finance.optimal_credit_length_estimation.detail.types import CreditCalculationResult
-from finance.optimal_credit_length_estimation.credit.simple_credit import calculate_credit, calculate_credit_with_overpayment
-from finance.optimal_credit_length_estimation.detail.visualization import plot_credit_results
-from finance.optimal_credit_length_estimation.credit.credit_with_investment import calculate_credit_with_investment
+from detail.input import parse_input, validate_input
+from detail.types import CreditCalculationResult
+from credit.simple_credit import (
+    calculate_credit,
+    calculate_credit_with_overpayment,
+    calculate_credit_with_investment,
+)
+from detail.visualization import plot_credit_results
 
 
 def print_credit_parameters(credit_parameters: dict[str, Any]) -> None:
