bdc26bb52124f072371b6e5a9c39dd99bbbf0383
Mikita <90976471+screenshifter@users.noreply.github.com>
2026-08-10 21:51:51 +0200

    Create Svelte app

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Hello world!</h1>
+<p>A new fancy Github page will appear here!</p>
+
+<style>
+    h1 {
+        color: #ff3e00;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        color: #333;
+        font-size: 1.2rem;
+    }
+</style>
