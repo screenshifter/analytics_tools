51f59bb55d466007359ce6ef0b43ccb48f364c8a
Mikita <90976471+screenshifter@users.noreply.github.com>
2026-08-11 20:56:30 +0200

    Add static application logo

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import app_icon from '../../static/logo.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={app_icon} />
 </svelte:head>
 
 {@render children()}
